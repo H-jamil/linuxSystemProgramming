53c86647241d25436b2c72cf1d19ff8e45850fc3
Jamil Hasibul <jamil@Jamils-MacBook-Pro-2.local>
2021-03-08 00:24:20 -0600

    added differentiation function to distinguish two data structure with callback

---
+++ ProgrammableLibrary/differentiation_callbacks/diff.c
@@ -0,0 +1,43 @@
+/**
+ * @Author: jamil
+ * @Date:   2021-02-18T07:48:49-06:00
+ * @Last modified by:   jamil
+ * @Last modified time: 2021-03-07T23:45:27-06:00
+ */
+
+
+
+#include <stdlib.h>
+
+/*Step 5 : */
+
+/*Return 0 if ds1 and ds2 are clones,
+ * else return -1*/
+int
+diff (void *root1,    /*pointer to Data structure 1*/
+      void *root2,    /*pointer to data structure 2*/
+      void *(*iterator(void *)), /*Iterator function callback*/
+      int comparator(void *, void *),
+      void *(*get_app_data(void *))){  /*Comparison function callback*/
+
+      void *next1 = NULL, *next2 = NULL;
+      /*Basic quick sanity checks*/
+      if(root1 && !root2)
+          return -1;
+      if(!root1 && root2)
+          return -1;
+      if(comparator(get_app_data(root1), get_app_data(root2)))
+          return -1;
+
+      next1 = iterator(root1);
+      next2 = iterator(root2);
+
+      while(1){
+        if(comparator(get_app_data(next1), get_app_data(next2)))
+            return -1;
+        next1 = iterator(next1);
+        next2 = iterator(next2);
+        if(!next1 && !next2) return 0;
+      }
+      return 0;
+}
